5f6861c68a078d88801af745890fe60e3d3be767
jonghyun <jonghyeon2046@gmail.com>
2022-09-16 14:00:45 +0900

    feat: 모든 일정 모달

---
+++ src/components/ShowAllEventsModal.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import useStore from '../store';
+import { getDateTimeFormat, getSimpleDateFormat } from '../utils/helper';
+import Modal from './Modal';
+
+interface Props {
+  onClose: (e: React.MouseEvent) => void;
+}
+const ShowAllEventsModal = ({ onClose }: Props) => {
+  const { events } = useStore();
+  return (
+    <Modal title="모든 일정 리스트" onClose={onClose}>
+      {events.map((event) => {
+        const eventStartDate = event.start.date ? event.start.date : event.start.dateTime;
+        const eventEndDate = event.end.date ? event.end.date : event.end.dateTime;
+        return (
+          <div className="mb-4 flex w-full flex-col items-start justify-center space-y-4 overflow-y-auto rounded-2xl bg-violet-500 p-2">
+            <span className="text-2xl">{event.summary}</span>
+            <span>{event.description}</span>
+            <span>{`${getSimpleDateFormat(new Date(eventStartDate as string))} ${getDateTimeFormat(
+              new Date(eventStartDate as string),
+            )} ~ ${getSimpleDateFormat(new Date(eventEndDate as string))} ${getDateTimeFormat(
+              new Date(eventEndDate as string),
+            )}`}</span>
+          </div>
+        );
+      })}
+    </Modal>
+  );
+};
+
+export default ShowAllEventsModal;
